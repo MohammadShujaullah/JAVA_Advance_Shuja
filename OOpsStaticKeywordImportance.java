0dd01b6f82a3db2f72668e1a6457ecf6bc800301
MohammadShujaullah <mdshujaullah789@gmail.com>
2025-10-04 00:00:27 +0530

    some modification in codes

--- OOpsStaticKeywordImportance.java
+++ OOpsStaticKeywordImportance.java
@@ -1,16 +1,30 @@
-class Student {
+/*The word static means "belongs to the class, not to any specific object".
+
+Normally, variables & methods belong to objects.
+But when you make something static, it belongs to the class itself, and all objects share it. */
+
+class student {
+    static int n = 20;
     String name;
     static String school;
 
-    public static void changeschool() {
+    public void changeschool() {
         school = "newschool";
+
     }
+
 }
 
 public class OOpsStaticKeywordImportance {
-    public static void main(String args[]) {
-        Student.school = "abc";
-        Student s1 = new Student();
-        System.out.println(s1.school);
+    public static void main(String[] args) {
+        student s1 = new student();
+        s1.name = "abc";
+        s1.school = "xyz";
+
+        System.out.println(s1.name);
+        System.out.println(student.school);
+        System.out.println(student.n);
+        s1.changeschool();
+        System.out.println(student.school);
     }
 }
